f187860ab2c86c53b0f922e1eaea17437bc0b1ee
vanessayukari <vaneyukari@outlook.com>
2025-04-04 13:02:24 -0300

    perfis implementados

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,16 +6,16 @@ import ProfileScreen from '../screens/ProfileScreen';
 const Stack = createNativeStackNavigator();
 
 export default function AppRoutes() {
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false,
-                animation: 'slide_from_right',
-            }}
-        >
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="CreateAppointment" component={CreateAppointmentScreen} />
-            <Stack.Screen name="Profile" component={ProfileScreen} />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        animation: 'slide_from_right',
+      }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="CreateAppointment" component={CreateAppointmentScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+    </Stack.Navigator>
+  );
 }
